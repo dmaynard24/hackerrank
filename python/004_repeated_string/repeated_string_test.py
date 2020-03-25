e6e165561bf809d2c4f09a2b0c3a0a81f763a3f6
Dave Maynard <davemaynard24@gmail.com>
2020-03-25 19:57:48 -0400

    ported solution 4 (Repeated String) to python

---
+++ python/004_repeated_string/repeated_string_test.py
@@ -0,0 +1,12 @@
+import unittest, repeated_string
+
+
+class TestRepeatedString(unittest.TestCase):
+  def test_repeated_string(self):
+    self.assertEqual(repeated_string.repeated_string('aba', 10), 7)
+    self.assertEqual(repeated_string.repeated_string('a', 1000000000000),
+                     1000000000000)
+
+
+if __name__ == '__main__':
+  unittest.main()
