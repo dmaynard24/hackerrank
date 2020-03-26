3801703121642e64a404139a0b0b6c17404fc354
Dave Maynard <davemaynard24@gmail.com>
2020-03-25 21:46:07 -0400

    added problem description and test suite to hourglass sum

---
+++ python/006_hourglass_sum/hourglass_sum.py
@@ -0,0 +1,84 @@
+# 2D Array
+
+# Given a 6 x 6 2D Array, arr:
+
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+
+# We define an hourglass in A to be a subset of values with indices falling in this pattern in arr's graphical representation:
+
+# a b c
+#   d
+# e f g
+
+# There are 16 hourglasses in arr, and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum.
+
+# For example, given the 2D array:
+
+# -9 -9 -9  1 1 1
+#  0 -9  0  4 3 2
+# -9 -9 -9  1 2 3
+#  0  0  8  6 6 0
+#  0  0  0 -2 0 0
+#  0  0  1  2 4 0
+
+# We calculate the following 16 hourglass values:
+
+# -63, -34, -9, 12,
+# -10, 0, 28, 23,
+# -27, -11, -2, 10,
+# 9, 17, 25, 18
+
+# Our highest hourglass value is 28 from the hourglass:
+
+# 0 4 3
+#   1
+# 8 6 6
+
+# Function Description
+
+# Complete the function hourglassSum in the editor below. It should return an integer, the maximum hourglass sum in the array.
+
+# hourglassSum has the following parameter(s):
+# arr: an array of integers
+
+# Input Format
+
+# Each of the 6 lines of inputs arr[i] contains 6 space-separated integers arr[i][j].
+
+# Constraints
+# -9 <= arr[i][j] <= 9
+# 0 <= i, j <= 5
+
+# Output Format
+
+# Print the largest (maximum) hourglass sum found in arr.
+
+# Sample Input
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 2 4 4 0
+# 0 0 0 2 0 0
+# 0 0 1 2 4 0
+
+# Sample Output
+# 19
+
+import sys
+
+
+def hourglass_sum(arr):
+  largest_sum = -sys.maxsize
+  max_y = len(arr) - 1
+  max_x = len(arr[0]) - 1
+  for y in range(1, max_y):
+    for x in range(1, max_x):
+      this_sum = arr[y - 1][x - 1] + arr[y - 1][x] + arr[y - 1][x + 1] + arr[
+          y][x] + arr[y + 1][x - 1] + arr[y + 1][x] + arr[y + 1][x + 1]
+      largest_sum = max(largest_sum, this_sum)
+  return largest_sum
