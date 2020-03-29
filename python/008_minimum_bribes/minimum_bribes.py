a1b5b2a0ccea45e1daa58993a89f675c8968f506
Dave Maynard <davemaynard24@gmail.com>
2020-03-29 14:48:25 -0400

    started problem 8

---
+++ python/008_minimum_bribes/minimum_bribes.py
@@ -0,0 +1,9 @@
+def minimum_bribes(q):
+  total_bribe_count = 0
+  return total_bribe_count
+
+
+minimum_bribes([2, 1, 5, 3, 4])  # 3
+minimum_bribes([2, 5, 1, 3, 4])  # Too chaotic
+minimum_bribes([1, 2, 5, 3, 4, 7, 8, 6])  # 4
+minimum_bribes([1, 2, 5, 3, 7, 8, 6, 4])  # 7
