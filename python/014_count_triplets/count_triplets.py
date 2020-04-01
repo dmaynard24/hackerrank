d6910654e201c28a7d4d465b448c759924644736
Dave Maynard <davemaynard24@gmail.com>
2020-03-31 20:20:21 -0400

    added an additional test case for problem 14

--- python/014_count_triplets/count_triplets.py
+++ python/014_count_triplets/count_triplets.py
@@ -1,14 +1,12 @@
 def countTriplets(arr, r):
+  count = 0
   val_counts = {}
   for val in arr:
     if val in val_counts:
       val_counts[val] += 1
     else:
       val_counts[val] = 1
-
-  count = 0
-  arr_len = len(arr)
-  for i in range(0, arr_len - 2):
+  for i in range(0, len(arr) - 2):
     if arr[i] * r in val_counts and arr[i] * r * r in val_counts:
       count += max(val_counts[arr[i] * r], val_counts[arr[i] * r * r])
   return count
@@ -16,3 +14,12 @@ def countTriplets(arr, r):
 
 print(countTriplets([1, 2, 2, 4], 2))  # 2
 print(countTriplets([1, 3, 9, 9, 27, 81], 3))  # 6
+print(countTriplets([1, 5, 5, 25, 125], 5))  # 4
+print(
+    countTriplets([
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1
+    ], 1))  # 161700
