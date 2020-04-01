f1fe5a87bd027f713c2bf53d52f1adef38c65037
Dave Maynard <davemaynard24@gmail.com>
2020-04-01 13:32:21 -0400

    solved problem 15 (Frequency Query) in python

--- python/015_frequency_query/frequency_query.py
+++ python/015_frequency_query/frequency_query.py
@@ -1,4 +1,4 @@
-def freqQuery(queries):
+def freq_query(queries):
   output = []
 
   data_counts = {}
@@ -6,14 +6,21 @@ def freqQuery(queries):
   for query in queries:
     [operation, data] = query
     if operation == 1:
-      data_counts[data] = data_counts.get(data, 0) + 1
+      if data in data_counts:
+        count_counts[data_counts[data]] = count_counts.get(
+            data_counts[data], 1) - 1
+        data_counts[data] += 1
+      else:
+        data_counts[data] = 1
       count_counts[data_counts[data]] = count_counts.get(data_counts[data],
                                                          0) + 1
     elif operation == 2:
-      data_counts[data] = max(0, data_counts.get(data, 1) - 1)
-      count_counts[data_counts[data]] = max(
-          0,
-          count_counts.get(data_counts[data], 1) - 1)
+      if data in data_counts and data_counts[data] > 0:
+        count_counts[data_counts[data]] = count_counts.get(
+            data_counts[data], 1) - 1
+        data_counts[data] -= 1
+        count_counts[data_counts[data]] = count_counts.get(
+            data_counts[data], 0) + 1
     elif operation == 3:
       if data in count_counts and count_counts[data] > 0:
         output.append(1)
@@ -23,7 +30,7 @@ def freqQuery(queries):
   return output
 
 
-print(freqQuery([
+print(freq_query([
     [1, 1],
     [2, 2],
     [3, 2],
@@ -34,9 +41,9 @@ print(freqQuery([
 ]))  # [0, 1]
 
 print(
-    freqQuery([[1, 5], [1, 6], [3, 2], [1, 10], [1, 10], [1, 6], [2, 5],
-               [3, 2]]))  # [0, 1]
+    freq_query([[1, 5], [1, 6], [3, 2], [1, 10], [1, 10], [1, 6], [2, 5],
+                [3, 2]]))  # [0, 1]
 
 print(
-    freqQuery([[1, 3], [2, 3], [3, 2], [1, 4], [1, 5], [1, 5], [1, 4], [3, 2],
-               [2, 4], [3, 2]]))  # [0, 1, 1]
+    freq_query([[1, 3], [2, 3], [3, 2], [1, 4], [1, 5], [1, 5], [1, 4], [3, 2],
+                [2, 4], [3, 2]]))  # [0, 1, 1]
