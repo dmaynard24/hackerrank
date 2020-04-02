75bd8b167c9cfae9b74138403886c0f22abe3a03
Dave Maynard <davemaynard24@gmail.com>
2020-04-02 15:03:38 -0400

    refactored solution to problem 19

--- python/019_activity_notifications/activity_notifications.py
+++ python/019_activity_notifications/activity_notifications.py
@@ -37,3 +37,4 @@ def activityNotifications(expenditure, d):
 
 print(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5))  # 2
 print(activityNotifications([1, 2, 3, 4, 4], 4))  # 0
+print(activityNotifications([10, 20, 30, 40, 50], 3))  # 1
