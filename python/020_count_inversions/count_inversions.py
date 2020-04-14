cf1fa83227d8b0ca9969d5917b752856958c2744
Dave Maynard <davemaynard24@gmail.com>
2020-04-14 13:08:42 -0400

    continued problem 20, trying to count inversions correctly

--- python/020_count_inversions/count_inversions.py
+++ python/020_count_inversions/count_inversions.py
@@ -2,16 +2,28 @@ def countInversions(arr):
   count = 0
 
   def merge_halves(left, middle, right):
+    temp = []
     left_start = left
     right_start = middle + 1
+    size = right - left + 1
 
     while left_start <= middle and right_start <= right:
       if arr[left_start] <= arr[right_start]:
+        temp.append(arr[left_start])
         left_start += 1
       else:
+        temp.append(arr[right_start])
         right_start += 1
+        nonlocal count
+        count += middle - left + 1 - left_start
 
-    # create temp and reconstruct here, counting inversions beforehand
+    if left_start <= middle:
+      temp.extend(arr[left_start:middle + 1])
+    else:
+      temp.extend(arr[right_start:right + 1])
+
+    arr[left:left + size] = temp
+    print(arr, count)
 
   def mergesort(left, right):
     if left < right:
@@ -25,6 +37,7 @@ def countInversions(arr):
   return count
 
 
-# print(countInversions([1, 1, 1, 2, 2]))  # 0
+print(countInversions([1, 1, 1, 2, 2]))  # 0
 print(countInversions([2, 1, 3, 1, 2]))  # 4
-# print(countInversions([7, 5, 3, 1])) # 6
+print(countInversions([7, 5, 3, 1]))  # 6
+print(countInversions([3, 2, 1]))  # 3
