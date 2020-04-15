a05fc174e48a08652a18dd29a05c0b0b37cf3dab
Dave Maynard <davemaynard24@gmail.com>
2020-04-15 11:51:36 -0400

    solved problem 20 (counting inversions)

--- python/020_count_inversions/count_inversions.py
+++ python/020_count_inversions/count_inversions.py
@@ -1,21 +1,19 @@
 def countInversions(arr):
-  count = 0
-
   def merge_halves(left, middle, right):
+    count = 0
     temp = []
     left_start = left
     right_start = middle + 1
     size = right - left + 1
 
     while left_start <= middle and right_start <= right:
-      if arr[left_start] <= arr[right_start]:
+      if arr[left_start] > arr[right_start]:
+        temp.append(arr[right_start])
+        count += middle - left_start + 1  # tricky
+        right_start += 1
+      else:
         temp.append(arr[left_start])
         left_start += 1
-      else:
-        temp.append(arr[right_start])
-        right_start += 1
-        nonlocal count
-        count += middle - left + 1 - left_start
 
     if left_start <= middle:
       temp.extend(arr[left_start:middle + 1])
@@ -23,21 +21,24 @@ def countInversions(arr):
       temp.extend(arr[right_start:right + 1])
 
     arr[left:left + size] = temp
-    print(arr, count)
+
+    return count
 
   def mergesort(left, right):
+    count = 0
+
     if left < right:
       middle = (left + right) // 2
-      mergesort(left, middle)
-      mergesort(middle + 1, right)
-      merge_halves(left, middle, right)
+      count += mergesort(left, middle)
+      count += mergesort(middle + 1, right)
+      count += merge_halves(left, middle, right)
 
-  mergesort(0, len(arr) - 1)
+    return count
 
-  return count
+  return mergesort(0, len(arr) - 1)
 
 
-print(countInversions([1, 1, 1, 2, 2]))  # 0
-print(countInversions([2, 1, 3, 1, 2]))  # 4
-print(countInversions([7, 5, 3, 1]))  # 6
-print(countInversions([3, 2, 1]))  # 3
+# print(countInversions([1, 1, 1, 2, 2]))  # 0
+# print(countInversions([2, 1, 3, 1, 2]))  # 4
+# print(countInversions([7, 5, 3, 1]))  # 6
+# print(countInversions([3, 2, 1]))  # 3
