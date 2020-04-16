f8f6e7f9176802fbbee768c1545a0d10716e0f54
Dave Maynard <davemaynard24@gmail.com>
2020-04-16 14:06:05 -0400

    optimized solution 24

--- python/024_substr_count/substr_count.py
+++ python/024_substr_count/substr_count.py
@@ -1,20 +1,20 @@
 def substrCount(n, s):
   count = n  # each individual char
 
-  def is_substr_special_palindrome(left, right):
-    same_char = s[left]
-    while left < right:
-      if s[left] != same_char or s[right] != same_char:
-        return False
-      left += 1
-      right -= 1
-
-    return True
-
-  for i in range(0, n):
-    for j in range(1, n - i):
-      if is_substr_special_palindrome(i, i + j):
-        count += 1
+  for i, char in enumerate(s):
+    different_char_index = None
+    for j in range(i + 1, n):
+      if char == s[j]:
+        if different_char_index is None:
+          count += 1
+        elif j - different_char_index == different_char_index - i:
+          count += 1
+          break
+      else:
+        if different_char_index is None:
+          different_char_index = j
+        else:
+          break
 
   return count
 
