84b969f4bb1b141c87cbcc68e2ce2bceba69c2d4
Dave Maynard <davemaynard24@gmail.com>
2020-04-20 17:13:13 -0400

    solved question 25 (common child) with dynamic programming

--- python/025_common_child/common_child.py
+++ python/025_common_child/common_child.py
@@ -1,30 +1,21 @@
 def commonChild(s1, s2):
-  longest_count = 0
-
-  s1_chars_indices = {}
-  for i, char in enumerate(list(s1)):
-    s1_chars_indices[char] = s1_chars_indices.get(char, []) + [i]
-
-  for i, char in enumerate(list(s2)):
-    if char in s1_chars_indices:
-      count = 1
-      prev_char_index = s1_chars_indices[char][0]
-      for j in range(i + 1, len(s2)):
-        if s2[j] in s1_chars_indices:
-          for k in s1_chars_indices[s2[j]]:
-            if k > prev_char_index:
-              count += 1
-              prev_char_index = k
-              break
-      longest_count = max(longest_count, count)
-
-  return longest_count
+  a = len(s1)
+  prev = [0] * (a + 1)
+  curr = [0] * (a + 1)
+  for i in reversed(range(a)):
+    for j in reversed(range(a)):
+      if s1[i] == s2[j]:
+        curr[j] = prev[j + 1] + 1
+      else:
+        curr[j] = max(curr[j + 1], prev[j])
+    prev, curr = curr, prev
+  return prev[0]
 
 
 print(commonChild('HARRY', 'SALLY'))  # 2
 print(commonChild('AA', 'BB'))  # 0
 print(commonChild('SHINCHAN', 'NOHARAAA'))  # 3
 print(commonChild('ABCDEF', 'FBDAMN'))  # 2
-# print(
-#     commonChild('WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS',
-#                 'FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC'))  # 15
+print(
+    commonChild('WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS',
+                'FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC'))  # 15
