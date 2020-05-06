b91a1eed40b4b94ccfdd798655f58de9fa1d9024
Dave Maynard <davemaynard24@gmail.com>
2020-05-05 20:42:18 -0400

    bugfix for Jumping on Clouds (revisited)

--- python/algorithms/implementation/jumping_on_clouds_revisited/jumping_on_clouds_revisited.py
+++ python/algorithms/implementation/jumping_on_clouds_revisited/jumping_on_clouds_revisited.py
@@ -1,9 +1,10 @@
 def jumping_on_clouds_revisited(c, k):
   n = len(c)
   e = 100
-  i = k
-  e -= 3 if c[i] == 1 else 1
-  while i != 0:
+  i = 0
+  while True:
     i = (i + k) % n
     e -= 3 if c[i] == 1 else 1
+    if i == 0:
+      break
   return e
