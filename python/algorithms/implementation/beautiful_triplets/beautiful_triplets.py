76940472fd99fcc2b267fdf488ca42d21ab049e8
Dave Maynard <davemaynard24@gmail.com>
2020-05-12 00:17:11 -0400

    optimized Beautiful Triplets with a dict

--- python/algorithms/implementation/beautiful_triplets/beautiful_triplets.py
+++ python/algorithms/implementation/beautiful_triplets/beautiful_triplets.py
@@ -1,13 +1,7 @@
 def beautiful_triplets(d, arr):
   count = 0
-  for i in range(len(arr) - 2):
-    for j in range(i + 1, len(arr) - 1):
-      if arr[j] - arr[i] > d:
-        break
-      if arr[j] - arr[i] == d:
-        for k in range(j + 1, len(arr)):
-          if arr[k] - arr[j] > d:
-            break
-          if arr[k] - arr[j] == d:
-            count += 1
+  nums = {num: 1 for num in arr}
+  for num in arr:
+    if num + d in nums and num + d * 2 in nums:
+      count += 1
   return count
