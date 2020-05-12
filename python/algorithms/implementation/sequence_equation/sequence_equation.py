7a578e44492514fcc508751294f76ed3d1ac27ee
Dave Maynard <davemaynard24@gmail.com>
2020-05-12 13:49:47 -0400

    refactored Sequence Equation with list comprehension

--- python/algorithms/implementation/sequence_equation/sequence_equation.py
+++ python/algorithms/implementation/sequence_equation/sequence_equation.py
@@ -1,8 +1,5 @@
 def sequence_equation(p):
-  result = []
   inverse = [None] * (len(p) + 1)
   for i, num in enumerate(p):
     inverse[num] = i + 1
-  for x in range(1, len(p) + 1):
-    result.append(inverse[inverse[x]])
-  return result
+  return [inverse[inverse[x]] for x in range(1, len(p) + 1)]
