6ab5dd701cea1f89a81aa72d18f80926a956e097
Dave Maynard <davemaynard24@gmail.com>
2020-05-12 00:40:49 -0400

    optimized Minimum Distance

--- python/algorithms/implementation/minimum_distances/minimum_distances.py
+++ python/algorithms/implementation/minimum_distances/minimum_distances.py
@@ -1,12 +1,9 @@
 def minimum_distances(a):
   l = len(a)
   md = l * 2
-  indices = {num: [] for num in a}
+  indices = {}
   for i, num in enumerate(a):
-    indices[num].append(i)
-  for v in indices.values():
-    if len(v) < 2:
-      continue
-    for i in range(1, len(v)):
-      md = min(md, v[i] - v[i - 1])
+    if num in indices:
+      md = min(md, i - indices.get(num))
+    indices[num] = i
   return md if md != l * 2 else -1
