2e113af7c8f05e2a38784e0603ce1f9372f4a0c5
Dave Maynard <davemaynard24@gmail.com>
2020-05-15 11:50:38 -0400

    solved Grid Search

--- python/algorithms/implementation/grid_search/grid_search.py
+++ python/algorithms/implementation/grid_search/grid_search.py
@@ -1,11 +1,12 @@
-def gridSearch(G, P):
-  for i in range(len(G) - len(P) + 1):
-    index = G[i].find(P[0])
-    if index == -1:
-      continue
-    for j in range(1, len(P)):
-      if G[i + j][index:index + len(P[j])] != P[j]:
-        break
-      elif j == len(P) - 1:
-        return 'YES'
+def grid_search(g, p):
+  for i in range(len(g) - len(p) + 1):
+    index = g[i].find(p[0], 0)
+    while index > -1:
+      for j in range(1, len(p)):
+        if g[i + j][index:index + len(p[j])] != p[j]:
+          break
+        elif j == len(p) - 1:
+          return 'YES'
+      # look for next index in same string
+      index = g[i].find(p[0], index + 1)
   return 'NO'
