77ba7e6e18674bf32fa91d3873c24a2564111f75
Dave Maynard <davemaynard24@gmail.com>
2020-05-17 18:57:25 -0400

    solved Absolute Permutation

--- python/algorithms/implementation/absolute_permutation/absolute_permutation.py
+++ python/algorithms/implementation/absolute_permutation/absolute_permutation.py
@@ -1,19 +1,14 @@
-import math
-
-
 def absolute_permutation(n, k):
   if k == 0:
     return list(range(1, n + 1))
   if n % 2 == 0 and k <= n / 2:
-    if k == n / 2:
-      return list(range(n // 2 + 1, n + 1)) + list(range(1, n // 2 + 1))
-    if (n % k == 0 and n / k % 2 == 0) or k == 1:
+    if n % k == 0 and n / k % 2 == 0:
       result = []
-      plus = True
+      is_plus = False
       for i in range(n):
-        if i > 0 and i % k == 0:
-          plus = not plus
-        if plus:
+        if i % k == 0:
+          is_plus = not is_plus
+        if is_plus:
           result.append((i + k) + 1)
         else:
           result.append((i - k) + 1)
