bab984e604542bacffe17bc491473e12e1757bd1
Dave Maynard <davemaynard24@gmail.com>
2020-05-20 11:23:58 -0400

    refactored Mars Exploration for readability

--- python/algorithms/strings/mars_exploration/mars_exploration.py
+++ python/algorithms/strings/mars_exploration/mars_exploration.py
@@ -1,10 +1,7 @@
 def mars_exploration(s):
   count = 0
+  expected = 'SOS'
   for i in range(len(s)):
-    if (i + 1) % 3 == 1 or (i + 1) % 3 == 0:
-      if s[i] != 'S':
-        count += 1
-    else:
-      if s[i] != 'O':
-        count += 1
+    if s[i] != expected[i % len(expected)]:
+      count += 1
   return count
--- python/algorithms/strings/mars_exploration/mars_exploration_test.py
+++ python/algorithms/strings/mars_exploration/mars_exploration_test.py
@@ -4,6 +4,8 @@ import unittest, mars_exploration
 class TestMarsExploration(unittest.TestCase):
   def test_mars_exploration(self):
     self.assertEqual(mars_exploration.mars_exploration('SOSSOT'), 1)
+    self.assertEqual(mars_exploration.mars_exploration('SOSSPSSQSSOR'), 3)
+    self.assertEqual(mars_exploration.mars_exploration('SOSSOSSOS'), 0)
 
 
 if __name__ == '__main__':
