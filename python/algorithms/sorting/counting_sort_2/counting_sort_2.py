b9fb42e3c973809d825febda30784e96be9e7ce1
Dave Maynard <davemaynard24@gmail.com>
2020-05-25 11:40:21 -0400

    optimized Counting Sort 2 by removing extra space

--- python/algorithms/sorting/counting_sort_2/counting_sort_2.py
+++ python/algorithms/sorting/counting_sort_2/counting_sort_2.py
@@ -2,7 +2,11 @@ def counting_sort_2(arr):
   counts = [0] * 100
   for num in arr:
     counts[num] += 1
-  sorted_arr = []
-  for i, count in enumerate(counts):
-    sorted_arr.extend([i] * count)
-  return sorted_arr
+
+  i = 0
+  for j in range(len(counts)):
+    count = counts[j]
+    arr[i:i + count] = [j] * count
+    i += count
+
+  return arr
