3192d9c8be9652288c6aef241b9a0a38dce2fe29
Dave Maynard <davemaynard24@gmail.com>
2020-05-28 15:10:33 -0400

    solved Separate the Numbers

---
+++ python/algorithms/strings/separate_numbers/separate_numbers.py
@@ -0,0 +1,26 @@
+def separateNumbers(s):
+  if s[0] == '0':
+    return 'NO'
+
+  def get_first_num(start, min_length, nums):
+    if start == len(s):
+      return nums[0]
+
+    for length in range(min_length, len(s) - start + 1):
+      num = int(s[start:start + length])
+      nums.append(num)
+      if nums[-1] - nums[-2] == 1:
+        return get_first_num(start + length, length, nums)
+      nums.pop()
+
+    return -1
+
+  for length in range(1, len(s) // 2 + 1):
+    num = get_first_num(length, length, [int(s[:length])])
+    if num != -1:
+      return 'YES ' + str(num)
+
+  return 'NO'
+
+
+print(separateNumbers('1234'))
