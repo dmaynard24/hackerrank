d89eb7631f608c4de7384e3fa96dabd5e86253cc
dmayna <dmayna@amazon.com>
2020-06-19 14:02:55 -0400

    escaped input in test case

--- python/practice/errors_and_exceptions/incorrect_regex/incorrect_regex_test.py
+++ python/practice/errors_and_exceptions/incorrect_regex/incorrect_regex_test.py
@@ -3,7 +3,7 @@ import unittest, incorrect_regex
 
 class TestIncorrectRegex(unittest.TestCase):
   def test_incorrect_regex(self):
-    self.assertEqual(incorrect_regex.incorrect_regex('.*\+'), True)
+    self.assertEqual(incorrect_regex.incorrect_regex('.*\\+'), True)
     self.assertEqual(incorrect_regex.incorrect_regex('.*+'), False)
 
 
