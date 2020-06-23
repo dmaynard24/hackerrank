467ea9f4b542392f1c457d548fa958076ea5492d
dmayna <dmayna@amazon.com>
2020-06-23 12:25:35 -0400

    updated Zipped to follow correct output format

--- python/practice/built_ins/zipped/zipped_test.py
+++ python/practice/built_ins/zipped/zipped_test.py
@@ -5,8 +5,11 @@ class TestZipped(unittest.TestCase):
   def test_zipped(self):
     self.assertEqual(
         zipped.zipped([[89, 90, 78, 93, 80], [90, 91, 85, 88, 86],
-                       [91, 92, 83, 89, 90.5]], 3),
-        [90.0, 91.0, 82.0, 90.0, 85.5])
+                       [91, 92, 83, 89, 90.5]], 3), '''90.0
+91.0
+82.0
+90.0
+85.5''')
 
 
 if __name__ == '__main__':
