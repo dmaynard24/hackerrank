f98d23f6f0eb42def8e04582f08f06139f913dd0
Dave Maynard <dmayna@amazon.com>
2020-07-13 16:12:46 -0400

    solved Eye and Identity

---
+++ python/practice/numpy/eye_and_identity/eye_and_identity.py
@@ -0,0 +1,5 @@
+import numpy
+
+
+def eye_and_identity(n, m):
+  return str(numpy.eye(n, m))
---
+++ python/practice/numpy/eye_and_identity/eye_and_identity_test.py
@@ -0,0 +1,12 @@
+import unittest, eye_and_identity
+
+
+class TestConcatenate(unittest.TestCase):
+  def test_eye_and_identity(self):
+    self.assertEqual(eye_and_identity.eye_and_identity(3, 3), '''[[1. 0. 0.]
+ [0. 1. 0.]
+ [0. 0. 1.]]''')
+
+
+if __name__ == '__main__':
+  unittest.main()
