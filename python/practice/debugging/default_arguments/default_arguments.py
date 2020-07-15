e942837f9d3e48fd3df0b2dae98aff23a29a2707
Dave Maynard <dmayna@amazon.com>
2020-07-15 11:38:50 -0400

    solved Default Arguments

---
+++ python/practice/debugging/default_arguments/default_arguments.py
@@ -0,0 +1,34 @@
+class EvenStream(object):
+  def __init__(self):
+    self.current = 0
+
+  def get_next(self):
+    to_return = self.current
+    self.current += 2
+    return to_return
+
+
+class OddStream(object):
+  def __init__(self):
+    self.current = 1
+
+  def get_next(self):
+    to_return = self.current
+    self.current += 2
+    return to_return
+
+
+def print_from_stream(n, stream=EvenStream()):
+  stream.__init__()
+  for _ in range(n):
+    print(stream.get_next())
+
+
+def default_arguments(queries):
+  for query in queries:
+    stream_name, n = query.split()
+    n = int(n)
+    if stream_name == 'even':
+      print_from_stream(n)
+    else:
+      print_from_stream(n, OddStream())
---
+++ python/practice/debugging/default_arguments/default_arguments_test.py
@@ -0,0 +1,24 @@
+from io import StringIO
+from unittest.mock import patch
+import unittest, default_arguments
+
+
+class TestTransposeFlatten(unittest.TestCase):
+  def test_default_arguments(self):
+    with patch('sys.stdout', new=StringIO()) as fake_out:
+      default_arguments.default_arguments(['odd 2', 'even 3', 'odd 5'])
+      self.assertEqual(fake_out.getvalue(), '''1
+3
+0
+2
+4
+1
+3
+5
+7
+9
+''')
+
+
+if __name__ == '__main__':
+  unittest.main()
