b172dfdb58e8387db1939eb90f29de99ce28cc90
Dave Maynard <davemaynard24@gmail.com>
2020-07-23 15:08:45 -0400

    moved Sock Merchant python module and separated test cases

--- python/algorithms/implementation/sock_merchant/sock_merchant_test.py
+++ python/algorithms/implementation/sock_merchant/sock_merchant_test.py
@@ -4,6 +4,8 @@ import unittest, sock_merchant
 class TestSockMerchant(unittest.TestCase):
   def test_sock_merchant(self):
     self.assertEqual(sock_merchant.sock_merchant(7, [1, 2, 1, 2, 1, 3, 2]), 2)
+
+  def test_sock_merchant_1(self):
     self.assertEqual(
         sock_merchant.sock_merchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]),
         3)
