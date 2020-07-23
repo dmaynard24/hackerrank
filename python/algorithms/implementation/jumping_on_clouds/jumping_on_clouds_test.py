9ab3be765b45905d18f01afae93ed620d119fac3
Dave Maynard <davemaynard24@gmail.com>
2020-07-23 15:22:56 -0400

    moved Repeated String, Jumping on Clouds

--- python/algorithms/implementation/jumping_on_clouds/jumping_on_clouds_test.py
+++ python/algorithms/implementation/jumping_on_clouds/jumping_on_clouds_test.py
@@ -5,6 +5,8 @@ class TestJumpingOnClouds(unittest.TestCase):
   def test_jumping_on_clouds(self):
     self.assertEqual(
         jumping_on_clouds.jumping_on_clouds([0, 0, 1, 0, 0, 1, 0]), 4)
+
+  def test_jumping_on_clouds_1(self):
     self.assertEqual(jumping_on_clouds.jumping_on_clouds([0, 0, 0, 0, 1, 0]),
                      3)
 
--- python/algorithms/implementation/repeated_string/repeated_string_test.py
+++ python/algorithms/implementation/repeated_string/repeated_string_test.py
@@ -4,6 +4,8 @@ import unittest, repeated_string
 class TestRepeatedString(unittest.TestCase):
   def test_repeated_string(self):
     self.assertEqual(repeated_string.repeated_string('aba', 10), 7)
+
+  def test_repeated_string_1(self):
     self.assertEqual(repeated_string.repeated_string('a', 1000000000000),
                      1000000000000)
 
