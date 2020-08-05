f10552c50a6ceb621de0a9d08ee8b6f32867d9bf
Dave Maynard <davemaynard24@gmail.com>
2020-08-04 21:46:31 -0400

    added more test cases to the Alternative Matching test suite

--- regex/practice/grouping_and_capturing/alternative_matching/alternative_matching_test.py
+++ regex/practice/grouping_and_capturing/alternative_matching/alternative_matching_test.py
@@ -2,9 +2,21 @@ import unittest, alternative_matching
 
 
 class TestAlternativeMatching(unittest.TestCase):
-  def alternative_matching(self):
+  def test_alternative_matching(self):
     self.assertEqual(alternative_matching.alternative_matching('Mr.DOSHI'),
                      'true')
+
+  def test_alternative_matching_1(self):
+    self.assertEqual(alternative_matching.alternative_matching('Mrs.DOSHI'),
+                     'true')
+
+  def test_alternative_matching_2(self):
+    self.assertEqual(alternative_matching.alternative_matching('Miss DOSHI'),
+                     'false')
+
+  def test_alternative_matching_3(self):
+    self.assertEqual(alternative_matching.alternative_matching('Dr. DOSHI'),
+                     'false')
 
 
 if __name__ == '__main__':
