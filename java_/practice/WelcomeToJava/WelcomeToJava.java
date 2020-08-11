5d3b7343e1d6a17393ed6f360a60994fb7a82281
Dave Maynard <davemaynard24@gmail.com>
2020-08-10 22:37:43 -0400

    solved Welcome to Java, still need to add unit test

---
+++ java_/practice/WelcomeToJava/WelcomeToJava.java
@@ -0,0 +1,8 @@
+package java_.practice.WelcomeToJava;
+
+public class WelcomeToJava {
+  public static void main(String[] args) {
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
+}
---
+++ java_/practice/WelcomeToJava/WelcomeToJavaTest.java
@@ -0,0 +1,7 @@
+package java_.practice.WelcomeToJava;
+
+public class WelcomeToJavaTest {
+  public boolean testWelcome() {
+    return true;
+  }
+}
